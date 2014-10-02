aa1ecc6ab07d7eb4f6b93e747bdf520327c396c6
root <root@onoo1.cafe24.com>
2014-10-02 11:21:36 +0900

    bug fixed

--- iosbetadeploy/urls.py
+++ iosbetadeploy/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, include, url
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 from iosbetadeploy.views import IndexView, ProjectInstanceView, FileRedirectView, InstanceFileView
-from localizable.views import LocalizableStringView
+#from localizable.views import LocalizableStringView
 from django.conf import settings
 from os import path
 admin.autodiscover()
@@ -21,12 +21,12 @@ urlpatterns = patterns('',
     url(r'^(?P<token>[\w\.]+)$', ProjectInstanceView.as_view(), name="project_view"),
     url(r'^(?P<token>[\w\.]+)/file_manager$', InstanceFileView.as_view(), name="instancefile_view"),
     url(r'^(?P<token>[\w\.]+)/(?P<key>[\w\-=]+)$', FileRedirectView.as_view(), name="file_redirection_view"),
-    url(r'^(?P<token>[\w\.]+)/localizable/(?P<locale>[\w\-=]+)$', LocalizableStringView.as_view()),
-    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': path.join(path.dirname(__file__), 'static')}),
-    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': "/home/uptown/db/media"}),
+#    url(r'^(?P<token>[\w\.]+)/localizable/(?P<locale>[\w\-=]+)$', LocalizableStringView.as_view()),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': './static'}),
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': "./media"}),
 
 )
 
-#from django.conf.urls.static import static
-#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-#urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# from django.conf.urls.static import static
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
